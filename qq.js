c7600b5d31f960117bb73392d987cd1916f75b68
daipaw0001 <31316610+daipaw0001@users.noreply.github.com>
2017-12-17 04:05:03 +0700

    Update qq.js

--- qq.js
+++ qq.js
@@ -1,22 +1,34 @@
 const puppeteer = require('puppeteer');
 
+function function2() {
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://teoxili.zohosites.com');
 
   // Get the "viewport" of the page, as reported by the page.
-while (1) {
-    
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio
-    };
-  });
+  
+          const browser = await puppeteer.launch();
+          const page = await browser.newPage();
+          await page.goto('http://teoxili.zohosites.com');
+          for (ii =0; ii < 30; ii++) {
+          const dimensions = await page.evaluate(() => {
+            return {
+              width: document.documentElement.clientWidth,
+              height: document.documentElement.clientHeight,
+              deviceScaleFactor: window.devicePixelRatio
+            };
+          });
 
-  console.log('Dimensions:', dimensions);
-}
-  await browser.close();
+          console.log('Dimensions:', dimensions);
+          }
+           await browser.close();
+     
 })();
+
+}
+ 
+var i = 0, howManyTimes = 10;
+function f() {
+    function2();
+    console.log(Date()); 
+    setTimeout( f, 50000 ); 
+}
+f();
