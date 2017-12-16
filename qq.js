0cfad09a2e0b7cb4f12588a21822e1a483f6cc9e
daipaw0001 <31316610+daipaw0001@users.noreply.github.com>
2017-12-17 03:45:17 +0700

    Create qq.js

---
+++ qq.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://teoxili.zohosites.com');
+
+  // Get the "viewport" of the page, as reported by the page.
+while (1) {
+    
+  const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio
+    };
+  });
+
+  console.log('Dimensions:', dimensions);
+}
+  await browser.close();
+})();
